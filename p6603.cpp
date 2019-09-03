9b0779a93dc39f31dff1b1ed687c5d1f710c340b
Jung Sim <tlawjd99gma@gmail.com>
2019-09-04 03:02:07 +0900

    p6603 solved

--- p6603.cpp
+++ p6603.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int arr
+int arr[14];
+int lts[7];
+int k;
+
+void lotto(int idx_lts, int idx_arr)
+{
+	if (idx_lts == 7)
+	{
+		for (int i = 1; i <= 6; i++)
+		{
+			cout << lts[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = idx_arr; i <= k; i++)
+	{
+		lts[idx_lts] = arr[i];
+		lotto(idx_lts + 1, i+1);
+	}
+}
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
-	int k;
 	
 	while (1)
 	{
@@ -17,10 +35,12 @@ int main()
 		if (k == 0)
 			break;
 
-		for (int i = 0; i < k; i++)
+		for (int i = 1; i <= k; i++)
 		{
-
+			cin >> arr[i];
 		}
+		lotto(1, 1);
+		cout << '\n';
 	}
 	return 0;
 }
