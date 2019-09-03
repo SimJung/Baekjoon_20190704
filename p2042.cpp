b7c14f412d5404e49062f9a694c759759fa4a6f3
Jung Sim <tlawjd99gma@gmail.com>
2019-09-03 16:26:29 +0900

    p2042 solving

---
+++ p2042.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int arr[1000001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m, k;
+	cin >> n>>m>>k;
+
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
+	
+
+	return 0;
+}
