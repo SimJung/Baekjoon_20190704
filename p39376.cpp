88d4ed36ac43a0c7ad07271081dc64354b3007bb
Jung Sim <tlawjd99gma@gmail.com>
2019-09-06 19:39:59 +0900

    programmers skill check test 2 solving

---
+++ p39376.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+	vector<vector<int>> answer;
+	vector<int> v;
+	int sum = 0;
+	for (int i = 0; i < arr1.size(); i++)
+	{
+		for (int j = 0; j < arr2[0].siz2(); j++)
+		{
+			for (int k = 0; k < arr2.size(); k++)
+			{
+				sum += arr1[]
+			}
+		}
+	}
+
+	return answer;
+}
