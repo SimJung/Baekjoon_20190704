8be41999aef22a0e5326ea8a42547490cd39a1be
Jung <tlawjd99gma@gmail.com>
2019-09-17 13:24:01 +0900

    p2743 solved

---
+++ p2743.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string a;
+	cin >> a;
+	cout << a.length() << '\n';
+	return 0;
+}
