1b81c9f05a0289684615292435945cbcd5a8bb79
Jung <tlawjd99gma@gmail.com>
2019-09-24 14:01:37 +0900

    p1485 solved

--- p1485.cpp
+++ p1485.cpp
@@ -14,30 +14,41 @@ int main()
 
 	while (t--)
 	{
-		int n = 0;
-		long long dist = -1;
+		int n = 0, nn=0, pos = 0;
+		long long arr[6] = { 0, };
 		for (int i = 0; i < 4; i++)
 		{
 			cin >> num[i][0] >> num[i][1];
 		}
 
+		long long dist = (num[1][0] - num[0][0])* (num[1][0] - num[0][0]) + (num[1][1] - num[0][1]) * (num[1][1] - num[0][1]);
+		long long crs;
 		for (int i = 0; i < 3; i++)
 		{
 			for (int j = i+1; j < 4; j++)
 			{
 				long long distt = (num[j][0] - num[i][0]) * (num[j][0] - num[i][0]) + (num[j][1] - num[i][1]) * (num[j][1] - num[i][1]);
-				if (dist == -1)
+				arr[pos] = distt;
+				if (dist > distt)
 				{
+					crs = dist;
 					dist = distt;
 				}
-				if (distt == dist)
-				{
-					n++;
+				if (distt > dist)
+					crs = distt;
+					
 
-				}
+				pos++;
 			}
 		}
-		if (n >= 4)
+		for (int i = 0; i < 6; i++)
+		{
+			if (arr[i] == dist)
+				n++;
+			else if (arr[i] == crs)
+				nn++;
+		}
+		if (n >= 4 && nn >= 2)
 			cout << 1 << '\n';
 		else
 			cout << 0 << '\n';
