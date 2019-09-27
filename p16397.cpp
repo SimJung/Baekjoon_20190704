e12706729ab39858f97261312ddfdb360849aa37
Jung <tlawjd99gma@gmail.com>
2019-09-27 13:31:51 +0900

    p16397 solved

--- p16397.cpp
+++ p16397.cpp
@@ -6,18 +6,21 @@ using namespace std;
 bool visit[100000];
 int tt[100000];
 queue<int> q;
-int n, t, g;
+int on, t, g;
+bool chk = true;
 
 int bfs() {
+	if (on == g)
+		return 0;
 	while (!q.empty())
 	{
 		int n = q.front();
 		q.pop();
 		if (n >= 50000 && g < 50000 && !tt[g])
-			return -1;
+			continue;
 		if (n == 99999 && !tt[g])
-			return -1;
-		if (n >= 100000)
+			continue;
+		if (n >= 100000 || tt[n] > t)
 			continue;
 
 		if (!visit[n+1] || tt[n+1] > tt[n]+1)
@@ -41,8 +44,16 @@ int bfs() {
 				q.push(new_n);
 			}
 		}
+		/*if (chk)
+		{
+			cout << n << endl;
+			cout << n+1 << " : " << tt[n+1] << '\n';
+			cout << new_n << " : " << tt[new_n] << "\n\n";
+			if (n == g)
+				chk = 0;
+		}*/
 	}
-	if (tt[g] > t)
+	if (tt[g] > t || tt[g] == 0)
 		return -1;
 	else
 		return tt[g];
@@ -55,12 +66,12 @@ int main()
 	cout.tie(NULL);
 
 	
-	cin >> n >> t >> g;
-	q.push(n);
-	visit[n] = 1;
+	cin >> on >> t >> g;
+	q.push(on);
+	visit[on] = 1;
 	int ans = bfs();
 
-	if (ans == -1)
+	if (ans == -1 || tt[g] > t)
 		cout << "ANG\n";
 	else
 		cout << ans<<'\n';
