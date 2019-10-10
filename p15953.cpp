16e48434c13da42c5717e50f049698f3e6fd1419
Jung Sim <tlawjd99gma@gmail.com>
2019-10-10 14:21:05 +0900

    p15953 solved

---
+++ p15953.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arr[101] = { 0, 500, 300, 300, 200, 200, 200, 50, 50, 50, 50, 30, 30, 30, 30, 30, 10, 10, 10, 10, 10, 10, };
+int arr2[65] = { 0, 512, 256, 256, 128, 128, 128, 128, 64, 64, 64, 64, 64, 64, 64, 64, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, };
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << (arr[a] + arr2[b])*10000 << '\n';
+	}
+	return 0;
+}
