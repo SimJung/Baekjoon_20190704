c6615fab7fc178cf7fcb94c588cebd340c81d08f
Jung Sim <tlawjd99gma@gmail.com>
2019-11-26 23:59:27 +0900

    p18108 solved

---
+++ p18108.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	cin >> t;
+
+	cout << t - (2541 - 1998) << '\n';
+	return 0;
+}
