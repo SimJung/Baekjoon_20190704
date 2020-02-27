8b94100cb0a52a14dab4b60447efc658d48e9def
Jung Sim <tlawjd99gma@gmail.com>
2020-02-28 06:35:54 +0900

    p11866 solved

---
+++ p11866.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+queue<int> q;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n,k;
+	cin >> n>>k;
+
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	cout << '<';
+	while (!q.empty()) {
+		for (int i = 0; i < k-1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front();
+		q.pop();
+		if (!q.empty())
+			cout << ", ";
+	}
+	cout << '>';
+	return 0;
+}
