c3a3f56f50b885d2b54edf163a7c1d3db1194e96
Jung Sim <tlawjd99gma@gmail.com>
2020-04-05 23:20:32 +0900

    p1654 solving . . .

---
+++ p1654.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+long long int arr[10001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long int cnt=0, n, k, l=0, r=0, m;
+	cin >> n>>k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		r += arr[i];
+	}
+
+
+	
+	while (l < r) {
+		cnt = 0;
+		m = (l + r) / 2;
+		if (m == 0){
+			m++;
+			break;
+		}
+
+		for (int i = 0; i < n; i++) {
+			cnt += arr[i] / m;
+			if (arr[i] < m)
+			{
+				cnt = 0;
+				break;
+			}
+		}
+
+		// 너무 길어!
+		if (cnt < k) {
+			r = m - 1;
+		}
+		//너무 짧아!
+		else {
+			l = m + 1;
+		}
+	}
+
+	cout << m << '\n';
+	return 0;
+}
