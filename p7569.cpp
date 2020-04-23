ccced49afdaef0fde18735ab6b7c5508e44293cf
Jung Sim <tlawjd99gma@gmail.com>
2020-04-23 20:20:35 +0900

    p7569 solved

--- p7569.cpp
+++ p7569.cpp
@@ -1,69 +1,62 @@
 #include <iostream>
-#include <queue>
 #include <tuple>
-#include <iomanip>
+#include <queue>
 using namespace std;
 #define ti tuple<int, int, int>
 
-int n, m, h, des, cur, ans;
+int n, m, h, ans = 0;
+//접근할 때는 높이, 세로, 가로 순으로
 int arr[101][101][101];
 bool visit[101][101][101];
 queue<ti> q;
 
-void showArr() {
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < m; j++) {
-			for (int k = 0; k < n; k++) {
-				cout << setw(6)<<arr[i][j][k] << ' ';
-			}
-			cout << '\n';
-		}
-		cout << "\n";
-	}
-	cout << "----------------------------------\n";
-}
-
 void bfs() {
 	while (!q.empty()) {
-		int x = get<0>(q.front()), y = get<1>(q.front()), z = get<2>(q.front());
+		// 0 번째 element, 높이
+		int th = get<0>(q.front());
+
+		// 1 번째 element, 세로
+		int ty = get<1>(q.front());
+
+		// 2 번째 element, 가로
+		int tx = get<2>(q.front());
 		q.pop();
-		cur++;
 
-		if (arr[x][y][z] - 1 > ans)
-			ans = arr[x][y][z] -1;
-
-		if (y > 0 && !visit[x][y - 1][z]) {
-			visit[x][y - 1][z] = 1;
-			q.push(make_tuple(x, y - 1, z));
-			arr[x][y - 1][z] = arr[x][y][z] + 1;
+		if (th > 0 && !visit[th - 1][ty][tx]) {
+			visit[th - 1][ty][tx] = 1;
+			arr[th - 1][ty][tx] = arr[th][ty][tx] + 1;
+			q.push(make_tuple(th - 1, ty, tx));
 		}
-		if (y < m-1 && !visit[x][y + 1][z]) {
-			visit[x][y + 1][z] = 1;
-			q.push(make_tuple(x, y + 1, z));
-			arr[x][y + 1][z] = arr[x][y][z] + 1;
-		}
-		if (x > 0 && !visit[x - 1][y][z]) {
-			visit[x - 1][y][z] = 1;
-			q.push(make_tuple(x - 1, y, z));
-			arr[x - 1][y][z] = arr[x][y][z] + 1;
-		}
-		if (x < h - 1 && !visit[x + 1][y][z]) {
-			visit[x + 1][y][z] = 1;
-			q.push(make_tuple(x + 1, y, z));
-			arr[x + 1][y][z] = arr[x][y][z] + 1;
-		}
-		if (z > 0 && !visit[x][y][z - 1]) {
-			visit[x][y][z - 1] = 1;
-			q.push(make_tuple(x, y, z - 1));
-			arr[x][y][z - 1] = arr[x][y][z] + 1;
-		}
-		if (z < n - 1 && !visit[x][y][z + 1]) {
-			visit[x][y][z + 1] = 1;
-			q.push(make_tuple(x, y, z + 1));
-			arr[x][y][z + 1] = arr[x][y][z] + 1;
+		if (th < h-1 && !visit[th + 1][ty][tx]) {
+			visit[th + 1][ty][tx] = 1;
+			arr[th + 1][ty][tx] = arr[th][ty][tx] + 1;
+			q.push(make_tuple(th + 1, ty, tx));
 		}
 
-		//showArr();
+		if (ty > 0 && !visit[th][ty - 1][tx]) {
+			visit[th][ty - 1][tx] = 1;
+			arr[th][ty - 1][tx] = arr[th][ty][tx] + 1;
+			q.push(make_tuple(th, ty - 1, tx));
+		}
+		if (ty < n - 1 && !visit[th][ty + 1][tx]) {
+			visit[th][ty + 1][tx] = 1;
+			arr[th][ty + 1][tx] = arr[th][ty][tx] + 1;
+			q.push(make_tuple(th, ty + 1, tx));
+		}
+
+		if (tx > 0 && !visit[th][ty][tx - 1]) {
+			visit[th][ty][tx - 1] = 1;
+			arr[th][ty][tx - 1] = arr[th][ty][tx] + 1;
+			q.push(make_tuple(th, ty, tx - 1));
+		}
+		if (tx < m - 1 && !visit[th][ty][tx + 1]) {
+			visit[th][ty][tx + 1] = 1;
+			arr[th][ty][tx + 1] = arr[th][ty][tx] + 1;
+			q.push(make_tuple(th, ty, tx + 1));
+		}
+
+		if (ans < arr[th][ty][tx] - 1)
+			ans = arr[th][ty][tx] - 1;
 	}
 }
 
@@ -73,39 +66,44 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> n>>m>>h;
-	des = n * m * h;
+	cin >> m >> n >> h;
 
+	
 	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < m; j++) {
-			for (int k = 0; k < n; k++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < m; k++) {
 				cin >> arr[i][j][k];
-				if (arr[i][j][k] == -1){
-					des--;
-					visit[i][j][k] = 1;
-				}
-				else if (arr[i][j][k] == 1){
-					cur++;
+
+				if (arr[i][j][k] == 1) {
 					q.push(make_tuple(i, j, k));
 					visit[i][j][k] = 1;
 				}
+				else if (arr[i][j][k] == -1)
+					visit[i][j][k] = 1;
 			}
 		}
 	}
 
-	
-	if (des == 0) {
+	if (q.empty()) {
 		cout << "-1\n";
-	}else if (cur == des) {
-		cout << "0\n";
 	}
 	else {
 		bfs();
-		if (cur >= des)
-			cout << ans << '\n';
-		else
-			cout << "-1\n";
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < m; k++) {
+					if (arr[i][j][k] == 0)
+					{
+						cout << "-1\n";
+						return 0;
+					}
+				}
+			}
+		}
+
+		cout << ans << '\n';
 	}
-	
+
 	return 0;
 }
