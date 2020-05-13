c05f2c06b3378928a06e322f15cb73bc68549e8f
Jung Sim <tlawjd99gma@gmail.com>
2020-05-13 23:10:34 +0900

    p12865 solving

--- p12865.cpp
+++ p12865.cpp
@@ -2,12 +2,10 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+#define pi pair<int, int>
 
-int weights[100001];
-
-bool cmp(pair<int, int> p1, pair<int, int> p2) {
-	return p1.first < p2.first;
-}
+int dp[100001];
+vector<pi> val;
 
 int main()
 {
@@ -18,30 +16,24 @@ int main()
 	int n, k, w, v, ans=0;
 	cin >> n >> k;
 
-	vector<pair<int, int>> pv;
-
 	for (int i = 0; i < n; i++) {
 		cin >> w >> v;
-		pv.push_back(make_pair(w, v));
-		weights[w] = v;
-		if (ans < v && w <= k)
-			ans = v;
+		val.push_back(make_pair(w, v));
 	}
 
-	sort(pv.begin(), pv.end());
-
 	for (int i = 0; i <= k; i++) {
-		if (i + pv[0].first > k) {
-			break;
-		}
 		for (int j = 0; j < n; j++) {
-			if (weights[i] + weights[pv[j].first] > weights[i + pv[j].first]) {
-				weights[pv[j].first + i] = weights[i] + weights[pv[j].first];
-				if (ans < weights[pv[j].first + i] && pv[j].first + i <= k)
-					ans = weights[pv[j].first + i];
+			if (i + val[j].first > k)
+				continue;
+
+			if (dp[i + val[j].first] < dp[i] + val[j].second) {
+				dp[i + val[j].first] = dp[i] + val[j].second;
+				if (ans < dp[i + val[j].first])
+					ans = dp[i + val[j].first];
 			}
 		}
 	}
+
 	cout << ans << '\n';
 	return 0;
 }
--- p1541.cpp
+++ p1541.cpp
@@ -1,86 +1,119 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-using namespace std;
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//
+//string a;
+//void sum(int idx) {
+//	int n, temp = idx, pVal, startIdx;
+//	while (temp > 0 && isdigit(a[temp-1]))
+//		temp--;
+//
+//	n = stoi(a.substr(temp, idx));
+//	startIdx = temp;
+//
+//	temp = idx + 1;
+//	idx++;
+//	while (true) {
+//		while (temp < a.length() && isdigit(a[temp]))
+//			temp++;
+//
+//		pVal = stoi(a.substr(idx, temp));
+//		n += pVal;
+//
+//		if (temp < a.length() && a[temp] == '+') {
+//			idx = temp + 1;
+//			temp = idx + 1;
+//		}
+//		else {
+//			break;
+//		}
+//	}
+//
+//	a.erase(a.begin() + startIdx, a.begin() + temp);
+//	a.insert(startIdx, to_string(n));
+//}
+//
+//int subtract() {
+//	int n, idx, temp = 0, pVal;
+//
+//	idx = a.find('-');
+//	if (idx != -1) {
+//		n = stoi(a.substr(temp, idx));
+//
+//		idx++;
+//		while (true) {
+//			temp = a.find('-', idx);
+//			if (temp == -1) {
+//				pVal = stoi(a.substr(idx, a.length() - idx));
+//				n -= pVal;
+//				break;
+//			}
+//			else {
+//				pVal = stoi(a.substr(idx, temp));
+//				n -= pVal;
+//
+//				idx = temp + 1;
+//			}
+//
+//			
+//		}
+//	}
+//	else {
+//		n = stoi(a);
+//	}
+//	
+//	return n;
+//}
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	cin >> a;
+//
+//	int idx = a.find('+');
+//	while (idx != -1) {
+//		sum(idx);
+//		idx = a.find('+');
+//	}
+//
+//	cout << subtract() << '\n';
+//	return 0;
+//}
 
-string a;
-void sum(int idx) {
-	int n, temp = idx, pVal, startIdx;
-	while (temp > 0 && isdigit(a[temp-1]))
-		temp--;
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
 
-	n = stoi(a.substr(temp, idx));
-	startIdx = temp;
-
-	temp = idx + 1;
-	idx++;
-	while (true) {
-		while (temp < a.length() && isdigit(a[temp]))
-			temp++;
-
-		pVal = stoi(a.substr(idx, temp));
-		n += pVal;
-
-		if (temp < a.length() && a[temp] == '+') {
-			idx = temp + 1;
-			temp = idx + 1;
-		}
-		else {
-			break;
-		}
-	}
-
-	a.erase(a.begin() + startIdx, a.begin() + temp);
-	a.insert(startIdx, to_string(n));
-}
-
-int subtract() {
-	int n, idx, temp = 0, pVal;
-
-	idx = a.find('-');
-	if (idx != -1) {
-		n = stoi(a.substr(temp, idx));
-
-		idx++;
-		while (true) {
-			temp = a.find('-', idx);
-			if (temp == -1) {
-				pVal = stoi(a.substr(idx, a.length() - idx));
-				n -= pVal;
-				break;
-			}
-			else {
-				pVal = stoi(a.substr(idx, temp));
-				n -= pVal;
-
-				idx = temp + 1;
-			}
-
-			
-		}
-	}
-	else {
-		n = stoi(a);
-	}
-	
-	return n;
-}
-
-int main()
+int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	char* arr;
+	int  sum = 0, cal, index = 1;
 
-	
-	cin >> a;
+	arr = (char*)malloc(sizeof(char*) * 51);
 
-	int idx = a.find('+');
-	while (idx != -1) {
-		sum(idx);
-		idx = a.find('+');
+	scanf_s("%s", arr);
+
+	while (1)
+	{
+		cal = strtol(arr, &arr, 10);
+		if (cal == NULL)
+			break;
+
+		if (index == 1)
+			sum += cal;
+		if (index == -1)
+			sum = sum - abs(cal);
+		else if (cal < 0)
+			index *= -1;
 	}
 
-	cout << subtract() << '\n';
+	printf("%d\n", sum);
+
 	return 0;
+
 }
